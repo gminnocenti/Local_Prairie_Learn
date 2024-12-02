7c1fe71db58868bb938949ff7be4d8c793f5aa5d
Gian Marco Innocenti <94134814+gminnocenti@users.noreply.github.com>
2024-12-01 20:24:42 -0800

    add image

--- questions/Animate-DFS/server.py
+++ questions/Animate-DFS/server.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def generate(data):
     mat=np.array([[0, 1, 0, 1, 0],
-                         [1, 0, 1, 1, 0],
+                         [1, 0, 1, 0, 0],
                          [0, 1, 0, 0, 1],
-                         [1, 1, 0, 0, 0],
+                         [1, 0, 0, 0, 0],
                          [0, 0, 1, 0, 0]])
 
     data["params"]["matrix"] = pl.to_json(mat)
